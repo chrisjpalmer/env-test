32fa3800cc77c02ee088d9e4f4fb3bc947b9153e
Chris Palmer <christopher.palmer@nine.com.au>
2025-09-26 09:06:12 +1000

    fix custom image script

--- scripts/custom-image.sh
+++ scripts/custom-image.sh
@@ -1,6 +1,6 @@
-dagger -m github.com/shykes/dagger@local-defaults <<EOF
+dagger -m github.com/shykes/dagger/cmd/engine@local-defaults <<EOF
 
     docker_sock=\$(host | unix-socket /var/run/docker.sock)
 
-    . | load-to-docker $docker_sock "custom-dagger-engine" "linux/arm64"
+    . | load-to-docker \$docker_sock --name "custom-dagger-engine" --platform "linux/arm64"
 EOF
